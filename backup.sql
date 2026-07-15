833add2117c5e360640c63627bad180ef92eb767
Yedekleme Botu <actions@github.com>
2026-07-15 01:19:53 +0000

    Otomatik Yedek: Wed Jul 15 01:19:53 UTC 2026

--- backup.sql
+++ backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict SRMHAKC1UZEg3O6ffsGtk8aQGTu7RcBfMCF9EZ0VVXvQAE8qquxiiM6BwpzzChh
+\restrict 792I2C6yEFh7VXplMRcN7rhGjCT8RsbCgSKoB9KSbfuEbBW1X4tcWvMrdQSUuMi
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.10 (Debian 17.10-1.pgdg13+1)
@@ -2492,5 +2492,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE supabase_admin IN SCHEMA public GRANT ALL ON T
 -- PostgreSQL database dump complete
 --
 
-\unrestrict SRMHAKC1UZEg3O6ffsGtk8aQGTu7RcBfMCF9EZ0VVXvQAE8qquxiiM6BwpzzChh
+\unrestrict 792I2C6yEFh7VXplMRcN7rhGjCT8RsbCgSKoB9KSbfuEbBW1X4tcWvMrdQSUuMi
 
